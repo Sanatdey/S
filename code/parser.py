fc51ec7ed6f517f004ecf83ef4499d2eb0bacbf2
gr33nm0nk2802 <gr33nm0nk2802@gmail.com>
2020-01-08 02:11:42 -0800

    Spelling Errors corrected and Variable names modified to meaningful names

--- code/parser.py
+++ code/parser.py
@@ -1,38 +1,44 @@
+
 import mymath
+
 class Par(object):
+
 	def __init__(self,tokens):
 		self.tokens=tokens
+
 	def parse(self):
-		self.count = 0
-		while self.count < len(self.tokens):
-			tokens_type = self.tokens[self.count][0]
-			tokens_value = self.tokens[self.count][1]
-			#print(tokens_type , tokens_value)
-			if tokens_type == 'VARIABLE' and tokens_value == 'var':
-				self.par_var_dec(self.tokens[self.count:len(self.tokens)])
-				ask = (mymath.assignmentOpp(self.tokens[self.count:len(self.tokens)]))
-			elif tokens_type == 'IDENTIFIER' and tokens_value == 'showme':
-				self.showme(self.tokens[self.count:len(self.tokens)],ask)
-			elif tokens_type == 'INOUT' and tokens_value == '~':
-				mymath.math(self.tokens[self.count:len(self.tokens)],ask)
+		self.token_index = 0
+		
+		while self.token_index < len(self.tokens):
+			tokens_type = self.tokens[self.token_index][0]
+			tokens_value = self.tokens[self.token_index][1]
 			
-			self.count += 1
-	def par_var_dec(self ,token_tip	):
+			if tokens_type == 'VARIABLE' and tokens_value == 'var':
+				self.par_var_dec(self.tokens[self.token_index:len(self.tokens)])
+				ask = (mymath.assignmentOpp(self.tokens[self.token_index:len(self.tokens)]))
+			elif tokens_type == 'IDENTIFIER' and tokens_value == 'showme':
+				self.showme(self.tokens[self.token_index:len(self.tokens)],ask)
+			elif tokens_type == 'INOUT' and tokens_value == '~':
+				mymath.math(self.tokens[self.token_index:len(self.tokens)],ask)
+			
+			self.token_index += 1
+
+	def par_var_dec(self ,token_ip_stream	):
 		token_chk = 0
-		for token in range(0,len(token_tip)+1):
-			tokens_type = token_tip[token_chk][0]
-			tokens_value = token_tip[token_chk][1]
+		for token in range(0,len(token_ip_stream)+1):
+			tokens_type = token_ip_stream[token_chk][0]
+			tokens_value = token_ip_stream[token_chk][1]
 			il=[]
-			for i in range(1,len(token_tip)+1,4):
+			for i in range(1,len(token_ip_stream)+1,4):
 				il.append(i)
 			ol=[]
-			for j in range(2,len(token_tip)+1,4):
+			for j in range(2,len(token_ip_stream)+1,4):
 				ol.append(j)
 			vl=[]
-			for h in range(3,len(token_tip)+1,4):
+			for h in range(3,len(token_ip_stream)+1,4):
 				vl.append(h)
 			cl=[]
-			for k in range(4,len(token_tip)+1,4):
+			for k in range(4,len(token_ip_stream)+1,4):
 				cl.append(k)
 			if tokens_type == 'STATEMENT_END':
 				break
@@ -41,14 +47,14 @@ class Par(object):
 				if tokens_type == "IDENTIFIER":
 					identifier = tokens_value
 				else :
-					print("Syntax ERROR : you must give a variable name after variable decliaration")		
+					print("Syntax ERROR : You must give a variable name after variable declaration")		
 					break
 			elif token in ol:#2 or token == 4 or token == 6 or token == 8:
 				if tokens_type == "OPERATOR":
 					tok =1
 					
 				else :
-					print("Syntax ERROR :you miss the assignment operator after VARIABLE name")
+					print("Syntax ERROR : You miss the assignment operator after VARIABLE name")
 					break
 			elif token in vl:#== 3 or token == 5 or token == 7 or token == 9:
 				if tokens_type == 'STRING':
@@ -56,7 +62,7 @@ class Par(object):
 				elif tokens_type == 'INTEGER':
 					initalization =1
 				else :
-					print("Syntax ERROR : IT CAN BE A INTEGER, STRING OR IDEMTIFIRE I.E. variable")
+					print("Syntax ERROR : IT CAN BE A INTEGER, STRING OR IDENTIFIER I.E. variable")
 					break
 			
 			elif token in cl:
@@ -65,29 +71,29 @@ class Par(object):
 				elif tokens_type == "STATEMENT_END":
 					break
 				else :
-					print("Syntax ERROR : in this position you can used comma for multiple decliaration or u can used assignment operator for initalization")
+					print("Syntax ERROR : In this position you can used comma for multiple declaration or you can used assignment operator for initalization")
 					break
 			token_chk = token_chk + 1 
 			
 		
 			
 			
-	def showme(self,token_tip,toko):
+	def showme(self,token_ip_stream,toko):
 	
-		if token_tip[1][0] == "INOUT" :
-			if token_tip[2][0] == "STRING":
-				print(token_tip[2][1])
-			elif token_tip[3][0]=="EMPTY":
-				print("Statement MISSING : you have to give '.' for debag")
+		if token_ip_stream[1][0] == "INOUT" :
+			if token_ip_stream[2][0] == "STRING":
+				print(token_ip_stream[2][1])
+			elif token_ip_stream[3][0]=="EMPTY":
+				print("Statement End MISSING : you have to give '.' for debug")
 			
 		else:
-			print("Syntax ERROR : you have to type string within parentisis or you have to give output operator")
+			print("Syntax ERROR : You have to type string within parenthesis or you have to give output operator")
 			
 		token_chk = 1
 		for i in range(0 ,len(toko)):
 			tokens_id=toko[i][0]
 			tokens_val=toko[i][1]
-			if token_tip[1][0] == "INOUT" and token_tip[2][1] == tokens_id and token_tip[3][0] == 'STATEMENT_END':
+			if token_ip_stream[1][0] == "INOUT" and token_ip_stream[2][1] == tokens_id and token_ip_stream[3][0] == 'STATEMENT_END':
 					print(tokens_val)
 			
 
