a05327abb2db10fd325c4ba753030eb7d3f27351
gr33nm0nk2802 <gr33nm0nk2802@gmail.com>
2020-01-08 01:30:19 -0800

    Syntax highlighting and code correction

--- code/main.py
+++ code/main.py
@@ -4,14 +4,31 @@ import lexer
 
 def main():
 
+	# Variable to store the contents of the file
 	content = ""
 
+
 	with open('test.lang', 'r') as file:
+		# reading and storing the contents of the file
 		content = file.read()
+
+	############################
+    #         Lexer           #
+    ############################
+
+	# Calling the lexer file Lexer class and initializing it with the source code in lex
 	lex = lexer.Lexer(content)
+	# Calling the tokenize method from inside the lexer instance
 	tokens = lex.tokenize()
+
+	############################
+    #         Parser           #
+    ############################
+
+	# Calling the parser file Parser class and initializing it with the source code
 	parse = parser.Par(tokens)
-	obj = parse.parse()
+	# Calling the parse method from inside the parse instance
+	parse.parse()
 
 
 main()
