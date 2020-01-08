9fab409a7c7d852d8105b79e9bfcd76f10347af1
gr33nm0nk2802 <gr33nm0nk2802@gmail.com>
2020-01-08 02:12:16 -0800

    Variable names midified to meaningful names

--- code/mymath.py
+++ code/mymath.py
@@ -1,46 +1,44 @@
-def math(token_tip,toko):
+def math(token_ip_stream,toko):
 	
-		if token_tip[0][0] == 'INOUT' and token_tip[1][0] == 'IDENTIFIRE' and token_tip[2][1] == "=" and  token_tip[3][1] == toko[0][0] and token_tip[5][1] == toko[1][0] and token_tip[6][0] == "STATEMENT_END" :
+		if token_ip_stream[0][0] == 'INOUT' and token_ip_stream[1][0] == 'IDENTIFIRE' and token_ip_stream[2][1] == "=" and  token_ip_stream[3][1] == toko[0][0] and token_ip_stream[5][1] == toko[1][0] and token_ip_stream[6][0] == "STATEMENT_END" :
 						
-				if token_tip[4][1] == "+":
+				if token_ip_stream[4][1] == "+":
 					toko[2][1] = int(toko[0][1]) + int(toko[1][1])
 					toko[-1][0]	= " "		
 
-				elif token_tip[4][1] == "-":
+				elif token_ip_stream[4][1] == "-":
 					toko[2][1] = int(toko[0][1]) - int(toko[1][1])
 					toko[-1][0]	= " "	
 					
-				elif token_tip[4][1] == "*":
+				elif token_ip_stream[4][1] == "*":
 					toko[2][1] = int(toko[0][1]) * int(toko[1][1])
 					toko[-1][0]	= " "
 					
-				elif token_tip[4][1] == "%":
+				elif token_ip_stream[4][1] == "%":
 					toko[2][1] = int(toko[0][1]) % int(toko[1][1])
 					toko[-1][0]	= " "
 					
-				elif token_tip[4][1] == "@":
+				elif token_ip_stream[4][1] == "@":
 					toko[2][1] = int(toko[0][1]) // int(toko[1][1])
 					toko[-1][0]	= " "
 				
-				elif token_tip[4][1] == "/":
+				elif token_ip_stream[4][1] == "/":
 					toko[2][1] = int(toko[0][1]) / int(toko[1][1])
 					toko[-1][0]	= " "
 
 				else :
-					print("Syntex ERROR : you miss the operator or you have wrong syntax [Ex. ~ num1 = a + b .] or Statement MISSING : you miss the Statement end notation '.'")
-		#else:		#	else:
-		#				print("Syntex ERROR :")		print(" ")
+					print("Syntax ERROR : you miss the operator or you have wrong syntax [Ex. ~ num1 = a + b .] or Statement MISSING : You miss the Statement end notation '.'")
 
 		return (toko)
-def assignmentOpp(token_tip):
+
+def assignmentOpp(token_ip_stream):
 		toko = []
 		token_chk = 1
 		for i in range(0 ,len(toko)):
 			tokens_id=toko[i][0]
 			tokens_val=toko[i][1]
-		for token in range(0,len(token_tip)):
-			if token_tip[token][1]== '=':
-				#token_tip[token-1][1] = token_tip[token+1][1]
-				toko.append([token_tip[token-1][1],token_tip[token +1][1]])
+		for token in range(0,len(token_ip_stream)):
+			if token_ip_stream[token][1]== '=':
+				toko.append([token_ip_stream[token-1][1],token_ip_stream[token +1][1]])
 		return (toko)
 	
